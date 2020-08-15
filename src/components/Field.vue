15a288e6f9aa9e0e7982d855b9716ee67c62ac8c
lusta.vm <lusta.vlad2001@gmail.com>
2020-08-15 10:54:41 +0300

    Версия с гавнобэкгарундом, диалогом и костылём на скрытие v-menu

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -1,15 +1,73 @@
 <template>
-  <div>
+  <div >
+<!--    {{checkArray(field.value)}}-->
 
-    <!--&lt;!&ndash;    {{field}}&ndash;&gt;{{field.value}} <h1>{{field}}</h1>-->
+    <!--    {{tempArr}}te-->
+
+    <div v-if="multiple">
+      <v-dialog max-width="364" persistent scrollable v-model="dialog">
+        <!--        {{field.value}}-->
+        <!--        :style="{height: field.items.length-15*65+'px'}" don't work-->
+        <v-card color="grey darken-3" height="auto">
+          <!--        <v-card-title class="headline">Use Google's location service?</v-card-title>-->
+
+          <v-card-text class="px-2 pt-3 pb-0">
+            <v-list color="grey darken-3" dark v-if="field.items.length>0">
+              <v-list-item @click="toggle" ripple>
+                <v-list-item-title>Выбрать все</v-list-item-title>
+                <v-list-item-action>
+                  <v-icon :color="field.value.length > 0 ? 'indigo lighten-4' : ''">{{icon}}</v-icon>
+                </v-list-item-action>
+              </v-list-item>
+            </v-list>
+            <v-card-title class="d-flex justify-center" v-if="field.items.length===0">
+              <span class="white--text">
+              Нет данных</span>
+
+            </v-card-title>
+            <v-list
+              color="grey darken-3"
+              dark
+              flat
+              subheader
+              two-line
+            >
+              <v-list-item-group
+                multiple
+              >
+                <v-list-item :key="j" @change="change(i.id,j)" v-for="(i,j) in field.items">
+                  <template v-slot:default>
+                    <v-list-item-content>
+                      <v-list-item-title>{{i.name}}</v-list-item-title>
+                      <!--                    <v-list-item-subtitle>Allow notifications</v-list-item-subtitle>-->
+                    </v-list-item-content>
+                    <v-list-item-action>
+                      <!--                      {{active}}-->
+                      <!--                      {{field.value[j]}}-->
+                      <v-checkbox
+                        :input-value="field.value[j]"
+                        color="primary"
+                      ></v-checkbox>
+                    </v-list-item-action>
+
+
+                  </template>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+          </v-card-text>
+          <v-card-actions class="pb-1 pt-0">
+            <v-spacer></v-spacer>
+            <v-btn @click="cancel()" color="primary" text>Отмена</v-btn>
+            <v-btn @click="accept()" color="primary" text>Ок</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
     <v-col class="pt-0" cols="12" md="12">
-      <v-select :clearable="iconInItem" :disabled="disabled && isEmpty" :items="field.items" :label="label" :multiple="multiple"
-                hide-details="auto" item-text="name" item-value="id" no-data-text="Нет данных" v-model="field.value" :rules="[required]">
-<!--                :rules="[-->
-<!--		v => !!v || 'Не может быть пустым',-->
-<!--		v => (v && v.length > 0) || 'Не может быть пустым'-->
-<!--	]">-->
-        <!--        Add a tile with Select All as Label and binded on a method that add or remove all items-->
+      <v-select :clearable="iconInItem" :disabled="disabled && isEmpty" :items="field.items" :label="label" :multiple="multiple" :rules="[required]"
+                @click="openDialog($event)" @click:clear="callBack()" hide-details="auto" item-text="name"
+                item-value="id" no-data-text="Нет данных" ref="select" v-model="field.value">
         <v-list-item @click="toggle" ripple slot="prepend-item" v-if="field.items.length>0 && multiple">
           <v-list-item-action>
             <v-icon :color="field.value.length > 0 ? 'indigo darken-4' : ''">{{icon}}</v-icon>
@@ -19,10 +77,6 @@
         <v-divider class="mt-2" slot="prepend-item" v-if="field.items.length>0 && multiple"/>
         <template v-if="iconInItem" v-slot:item="{ item}">
           <div style="display: flex;align-items: center">
-            <!--            <v-checkbox-->
-
-            <!--              color="accent"-->
-            <!--            ></v-checkbox>-->
             <v-img :src="item.link" class="mr-1" height="25px" v-if="item.id>0" width="25px"></v-img>
             <span>{{ item.name }}</span>
           </div>
@@ -34,10 +88,8 @@
             <span>{{ item.name }}</span>
             <span v-if="index<field.value.length-1 && index !==1">, </span>
             </span>
-            <span
-              class="grey--text caption"
-              v-if="index === 2"
-            >(+ ещё {{ field.value.length - 2 }})</span>
+            <span class="grey--text caption" v-if="index === 2 && item!==''">(+ ещё {{ checkArray-2}})</span>
+            <!--            <span class="grey&#45;&#45;text caption" v-if="index === 2 && item!==''">(+ ещё {{ field.value.length - 2 }})</span>-->
           </div>
         </template>
       </v-select>
@@ -82,39 +134,77 @@ export default {
 		itemValue: '',
 	},
 	mounted() {
-		console.log(this.field, 'field')
 	},
 	data: () => ({
-		// fullName: [
-		// 	v => !!v || 'Не может быть пустым',
-		// 	// v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-		// ],
+		dialog: false,
+		tempArr: [],//for cancel or accept methods /
 	}),
 	methods: {
+		change(id, index) {
+			this.field.value[index] ? this.$set(this.field.value, index, '') : this.$set(this.field.value, index, id)
+			// this.field.value[index]=id
+		},
+		cancel() {
+			// this.field.value = []
+			this.dialog = false
+			this.field.value = this.tempArr
+			// this.field.value.filter((e) => {
+			// 	console.log(this.tempArr.indexOf(e),'E')
+			// 	return this.tempArr.indexOf(e) > 0
+			// })
+		},
+		accept() {
+			this.tempArr = this.field.value.slice()
+			this.dialog = false
+			this.$forceUpdate()
+		},
+		openDialog() {
+			// костыль, но работает
+			// this.$refs.select.$children[1].styles.display='none'
+			// console.log(this.$refs.select.$children[1])
+			if (this.multiple)
+				// setTimeout(() => {
+				// 	this.$refs.select.$children[1].$refs.content.style.display = 'none'
+				// }, 155)
+				// this.$refs.select.$children[1].$el.style['display']='none'
+				if (this.field.items.length > 0)
+					this.multiple ? this.dialog = true : ''
+			// document.querySelector('.v-menu__content').style.display='none'
+		},
+		callBack() {
+			this.$emit('getModel')
+		},
 		required(value) {
 			if (value instanceof Array && value.length === 0) {
-				return 'Не может быть пустым';
+				return 'Не может быть пустым'
 			}
-			if(value===0){
+			if (value === 0) {
 				return !value
-      }
-			return !!value || 'Не может быть пустым';
+			}
+			return !!value || 'Не может быть пустым'
 		},
 		toggle() {
-			console.log('toggle')
 			this.$nextTick(() => {
 				if (this.selectedAllGroup) {
 					this.field.value = []
 				} else {
-					// todo check
-					this.field.value = this.field.items.map(e => e.id).slice()
+					this.field.value = this.field.items.map(e => {return e.id}).slice()
 				}
 			})
 		},
 	},
 	computed: {
+		checkArray() {
+			let length = 0
+			for (let i = 0; i < this.field.value.length; i++) {
+				if (this.field.value[i]) length++
+			}
+			return length
+		},
+		tr() {
+			return this.field
+		},
 		test() {
-			console.log('testComputedInField')
 			if (this.model) {
 				return this.model
 			} else {
@@ -140,7 +230,6 @@ export default {
 				}
 				if (typeof vm.auto.value === "number") {
 					// // todo id
-					console.log('hm')
 					if (vm.auto.value === 0) {
 						vm.$emit('selectAll', vm)
 						return true
@@ -152,12 +241,132 @@ export default {
 					return true
 			}
 		}
-		// checkEmpty(){
-		//
-		// }
+	},
+	watch: {
+		dialog: function () {
+			if (this.dialog) {
+				// window.focus()
+				// document.querySelector('.v-menu__content').style.display='none'
+				// console.log(document.querySelector('.v-menu__content'))
+				// this.$refs.select.$refs.menu.$el.classList.add('.v-menu__content')
+				// console.log(this.$refs.select.$refs.menu.$el)
+				setTimeout(() => {
+					// this.$refs.select.$refs.menu.$refs.style.display = 'none'
+					this.$refs.select.$children[1].$refs.content.style.display = 'none'
+				}, 300)
+			}
+		}
 	},
 }
 </script>
 
-<style scoped>
+<style>
+.custom-select, .v-input__control {
+  cursor: pointer !important;
+}
+
+.v-list-item__title, .v-list-item__subtitle {
+  white-space: normal !important;
+}
+
+/*.v-menu__content{*/
+/*  display:none !important;*/
+/*}*/
+/*.v-menu__content,.theme--light,.menuable__content__active{*/
+/*  display:none !important;*/
+/*}*/
+
+
+@keyframes move-background {
+  from {
+    -webkit-transform: translate3d(0px, 0px, 0px);
+  }
+  to {
+    -webkit-transform: translate3d(1000px, 0px, 0px);
+  }
+}
+@-webkit-keyframes move-background {
+  from {
+    -webkit-transform: translate3d(0px, 0px, 0px);
+  }
+  to {
+    -webkit-transform: translate3d(1000px, 0px, 0px);
+  }
+}
+
+@-moz-keyframes move-background {
+  from {
+    -webkit-transform: translate3d(0px, 0px, 0px);
+  }
+  to {
+    -webkit-transform: translate3d(1000px, 0px, 0px);
+  }
+}
+
+@-webkit-keyframes move-background {
+  from {
+    -webkit-transform: translate3d(0px, 0px, 0px);
+  }
+  to {
+    -webkit-transform: translate3d(1000px, 0px, 0px);
+  }
+}
+
+
+
+.stars {
+  background: black url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/stars.png) repeat;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: block;
+  z-index: 0;
+}
+
+.twinkling{
+  width:10000px;
+  height: 100%;
+  background: transparent url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/twinkling.png") repeat;
+  background-size: 1000px 1000px;
+  position: absolute;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: 2;
+
+  -moz-animation:move-background 70s linear infinite;
+  -ms-animation:move-background 70s linear infinite;
+  -o-animation:move-background 70s linear infinite;
+  -webkit-animation:move-background 70s linear infinite;
+  animation:move-background 70s linear infinite;
+
+}
+
+.clouds{
+  width:10000px;
+  height: 100%;
+  background: transparent url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/clouds_repeat.png") repeat;
+  background-size: 1000px 1000px;
+  position: absolute;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: 3;
+
+  -moz-animation:move-background 150s linear infinite;
+  -ms-animation:move-background 150s linear infinite;
+  -o-animation:move-background 150s linear infinite;
+  -webkit-animation:move-background 150s linear infinite;
+  animation:move-background 150s linear infinite;
+}
+img{
+  height: 70vh;
+  width:70vh;
+  position: absolute;
+  z-index: 3;
+  right: 20px;
+}
+
 </style>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -3,24 +3,38 @@
     <v-row class="text-center">
       <v-col cols="12">
         <!--        <v-img src="https://www.carlogos.org/logo/Volkswagen-logo-2015-1920x1080.png"></v-img>-->
-        <v-card class="mx-auto" max-width="344">
+        <v-card class="mx-auto" max-width="364">
           <v-card-text>
+            <div class="d-inline-flex justify-center">
+              <v-img height="50" src="@/../public/11.png" width="50"/>
+            </div>
             <div class="headline text--primary">Регистрация поставщика</div>
-            <p>Введите данные вашей компании, чтобы зарегестрироваться в боте для получения доступа к заказам.</p>
+            <p class="mb-1">Введите данные вашей компании, чтобы получить доступ к заказам.</p>
+
             <!--            ref="form" v-model="valid"-->
             <v-form ref="form" v-model="valid">
               <v-row>
                 <!--                FIO-->
+                <v-col class="pb-0 pt-1" cols="12" md="12">
+                  <!--                  :rules="rules.fullName"-->
+                  <v-text-field
+                    :rules="rules.fullName"
+                    hide-details="auto"
+                    label="Введите ваше ФИО"
+                    outlined
+                    required
+                    v-model="fullName"
+                  ></v-text-field>
+                </v-col>
                 <v-col class="pb-0" cols="12" md="12">
                   <!--                  :rules="rules.fullName"-->
-                  <!--                  <v-text-field-->
-                  <!--                    :rules="rules.fullName"-->
-                  <!--                    hide-details="auto"-->
-                  <!--                    label="ФИО"-->
-                  <!--                    outlined-->
-                  <!--                    required-->
-                  <!--                    v-model="fullName"-->
-                  <!--                  ></v-text-field>-->
+                  <v-text-field
+                    disabled
+                    hide-details="auto"
+                    label="Введите Номер телефона"
+                    outlined
+                    v-model="phoneNumber"
+                  ></v-text-field>
                 </v-col>
                 <!--                Comppany-->
                 <v-col class="pb-0" cols="12" md="12">
@@ -28,7 +42,7 @@
                   <v-text-field
                     :rules="rules.companyName"
                     hide-details="auto"
-                    label="Название компании"
+                    label="Введите Название компании"
                     outlined
                     required
                     v-model="companyName"
@@ -40,7 +54,7 @@
                   <v-text-field
                     :rules="rules.cityName"
                     hide-details="auto"
-                    label="Город"
+                    label="Введите Город"
                     outlined
                     required
                     v-model="cityName"
@@ -52,7 +66,7 @@
                   <v-text-field
                     :rules="rules.address"
                     hide-details="auto"
-                    label="Адрес"
+                    label="Введите Адрес склада"
                     outlined
                     required
                     v-model="address"
@@ -61,6 +75,12 @@
                 <!--                auto parts-->
                 <v-col class="pb-0" cols="12" md="12">
                   <Field :field="carParts" label="Выберите автозапчасти"></Field>
+                </v-col>
+                <v-col class="pb-0" cols="12" md="12">
+                  <v-layout align-center justify-space-around>
+                    <v-checkbox :rules="[this.buAuto || this.newAuto]" class="mt-0 pt-0" hide-details label="Б/у" v-model="buAuto"></v-checkbox>
+                    <v-checkbox :rules="[this.buAuto || this.newAuto]" class="mt-0 pt-0" hide-details label="Новые" v-model="newAuto"></v-checkbox>
+                  </v-layout>
                 </v-col>
                 <v-col class="pa-0 mt-4" cols="12">
                   <v-card :key="j" class="mb-4" elevation="5" v-for="(i,j) in groups">
@@ -71,26 +91,24 @@
                           <v-icon>mdi-close</v-icon>
                         </v-btn>
                       </div>
-                      <Field :field="i.auto" :iconInItem="true" :multiple="false" label="Выберите автомобиль"></Field>
-                      <Field :auto="i.auto" :disabled="true" :field="i.models" @getModel="getModel($event,j)" @selectAll="selectAll($event,j)" label="Выберите модели"></Field>
+                      <Field :field="i.auto" :iconInItem="true" :key="i.auto.items[j].id" :multiple="false" @getModel="getModel($event,j)" label="Выберите автомобиль"></Field>
+                      <Field :auto="i.auto" :disabled="true" :field="i.models" :multiple="true" :key="i.models[j]" @getModel="getModel($event,j)" @selectAll="selectAll($event,j)"
+                             label="Выберите модели"></Field>
                     </v-card-text>
                   </v-card>
                   <v-btn :disabled="disabled" @click="addGroup" color="primary" small>
-                    <span class="caption text-none font-weight-medium">Добавить марку</span>
+                    <span class="caption text-none font-weight-medium">Добавить марку авто</span>
                   </v-btn>
                 </v-col>
                 <v-col cols="12">
-                  <v-layout align-center>
+                  <v-layout align-center justify-center class="pb-2">
                     <v-checkbox class="mt-0 pt-0" hide-details v-model="checkbox"></v-checkbox>
                     <a href="https://telegra.ph/Soglasie-na-obrabotku-dannyh-08-09" target="_blank">Даю согласие на обработку данных</a>
                   </v-layout>
-                  <!--                  :disabled="!valid"-->
                   <v-btn :disabled="!valid || !checkbox" @click="submit" class="mt-1" color="primary">Подтвердить</v-btn>
                 </v-col>
               </v-row>
             </v-form>
-            <!--            {{rules[0]}}-->
-            <!--            <v-select :items="items1" :rules="rules" clearable multiple v-model="value1"></v-select>-->
           </v-card-text>
         </v-card>
       </v-col>
@@ -107,10 +125,10 @@ export default {
 	async mounted() {
 		console.log(this.$route)
 		try {
-			await Promise.all([this.$API.car.parts(), this.$API.car.maker()]).then(values => {
+			await Promise.all([this.$API.car.parts(), this.$API.car.maker(), this.$API.car.phone(this.$route.params.id)]).then(values => {
 				this.carParts.items = values[0].data.slice()
 				this.carMaker.items = values[1].data.slice()
-				// this.carModel.items = values[2].data.slice()
+				this.phoneNumber = values[2].data.phone
 			})
 		} catch (e) {
 			console.error(e, 'error')
@@ -121,10 +139,9 @@ export default {
 	data: () => ({
 		valid: true,
 		rules: {
-			// fullName: [
-			// 	v => !!v || 'Не может быть пустым',
-			// 	// v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-			// ],
+			fullName: [
+				v => !!v || 'Не может быть пустым',
+			],
 			companyName: [
 				v => !!v || 'Не может быть пустым',
 			],
@@ -134,9 +151,16 @@ export default {
 			address: [
 				v => !!v || 'Не может быть пустым',
 			],
+			buAuto: [
+				v => !!v || 'Не может быть пустым',
+			],
+			newAuto: [
+				v => !!v || 'Не может быть пустым',
+			]
 		},
-		// fullName: '',
+		fullName: '',
 		companyName: '',
+		phoneNumber: '',
 		cityName: '',
 		address: '',
 		carMaker: { items: [], value: [] },
@@ -147,64 +171,64 @@ export default {
 		dialog: false,
 		groups: [],
 		indexes: [],
+		buAuto: false,
+		newAuto: false,
 	}),
 	methods: {
 		async submit() {
 			if (this.$refs.form.validate()) {
 				let test = []
-				console.log(this.groups, 'ALLLLO')
 				this.groups.forEach(e => {
+					if(e)
 					test.push(...e.models.value)
 				})
 				const payload = {
-					// full_name: this.fullName,
+					full_name: this.fullName,
 					company_name: this.companyName,
 					city_name: this.cityName,
 					address: this.address,
-					wt_list: this.carParts.value,
+					wt_list: this.carParts.value.filter(e=>e),
 					carmodel_list: test,
-					user_id: this.$route.params.id
+					user_id: this.$route.params.id,
+          state_auto: {buAuto:this.buAuto,newAuto:this.newAuto},
+          phone_number: this.phone_number
 				}
 				try {
 					await this.$API.car.addUser(payload)
-          alert('Регистрация прошла успешно')
+					alert('Регистрация прошла успешно')
+					this.resetForm()
 				} catch {
-          console.error('error')
+					alert('Произошла ошибка')
+					console.error('error')
 				}
 			}
 		},
+		resetForm() {
+			this.$refs.form.reset()
+		},
 		async getModel(carId, fieldIndex) {
-			console.log('indexes')
+			if (!carId && this.groups.length===1) {
+				this.groups.forEach(e => e.auto.items = JSON.parse(JSON.stringify(this.carMaker.items)))
+				return
+			}
+			if (this.groups[fieldIndex].models.value.indexOf(0) !== -1) {
+				this.groups[fieldIndex].models.value = []
+			}
 			this.indexes[fieldIndex] = carId
-			if (this.indexes.length > 0) this.carMaker.items.splice(0, 1)
-			// todo вроде получилось
-			this.groups.forEach((e, i) => {
-				if (!this.indexes[i]) {
-					e.auto.items = JSON.parse(JSON.stringify(this.carMaker.items.filter((e, i) => {
-						console.log(e, i, 'carMaker')
-						return this.indexes.find(el => {
-							console.log(el === e.id, 'find')
-							return el !== e.id
-						})
-					})))
+			if (this.indexes.length > 0) {
+				for (let i = 0; i < this.groups.length; i++) {
+					this.groups[i].auto.items = JSON.parse(JSON.stringify(this.carMaker.items.filter(e => e.id)))
 				}
-				console.log(e.auto.items, 'lolol')
-			})
-			this.groups[fieldIndex].models.value = []
-			// todo восстановление
-			// for (let i = 0; i < this.indexes.length; i++) {
-			// 	if (this.indexes[i]) {
-			// 		console.log(this.indexes[i])
-			// 		this.groups.forEach((e, index) => {
-			// 			console.log(e.auto.items[index], this.indexes[i], 'YES')
-			// 			if (e.auto.items[index].id === this.indexes[i]) {
-			// 				e.auto.items.splice(index, 1)
-			// 				console.log(this.groups, 'E')
-			// 			}
-			// 		})
-			// 	}
-			// }
-			console.log('CHTO', this.groups)
+			} else {
+				for (let i = 0; i < this.groups.length; i++) {
+					this.groups[i].auto.items = JSON.parse(JSON.stringify(this.carMaker.items))
+				}
+			}
+			if (this.indexes.filter(e => e === carId).length > 1) {
+				this.groups.splice(fieldIndex, 1)
+				this.indexes.splice(fieldIndex, 1)
+				return
+			}
 			const result = await this.$API.car.model(carId)
 			this.groups[fieldIndex].models.items = result.data
 		},
@@ -219,16 +243,12 @@ export default {
 		},
 		selectAll(e, index) {
 			this.groups[index].models.value = []
-			console.log(index, e, this.groups[index].models.value, 'EJ')
 			this.groups[index].models.value.push(0)
-			// this.groups[index].auto
 		}
 	},
 	computed: {
 		disabled: (vm) => {
-			// const carMaker = JSON.parse(JSON.stringify(vm.carMaker.items))
-			// check
-			if (vm.groups.length === vm.carMakerLength - 1) {
+			if (vm.groups.length === vm.carMakerLength) {
 				return true
 			}
 			if (vm.groups.length !== 0) {
@@ -240,10 +260,6 @@ export default {
 			}
 			return false
 		},
-		test: (vm) => {
-			console.warn('hmm')
-			return vm.groups
-		}
 	},
 	// watch: {
 	// 	groups: {
